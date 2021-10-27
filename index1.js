3befe4943c1ae9b9db57695b695b73cca9b92dfe
Abdalazez-Azmi <90723420+Abdalazez-Azmi@users.noreply.github.com>
2021-10-27 18:21:29 +0300

    edit

--- index1.js
+++ index1.js
@@ -273,15 +273,15 @@ window.onload= $(document).ready(function(){
   }
 })
 
-window.onload= $(document).ready(function(){
+// window.onload= $(document).ready(function(){
 
-  for (let i = 0; i < products_animate.length; i++) {
-    $(products_animate[i]).hide()
-    $(products_animate[i]).show(10000)
+//   for (let i = 0; i < products_animate.length; i++) {
+//     $(products_animate[i]).hide()
+//     $(products_animate[i]).show(10000)
 
     
-  }
-})
+//   }
+// })
 
 
 
@@ -406,10 +406,25 @@ const Additems = () => {
       Price: var1[i].value,
       Count: 0,
     });
-    countOfItem[i].innerHTML = 0;
+for (const key of arr_items) {
+  countOfItem[i].innerHTML = key.Count
+  console.log(key)+
+  
+
+
+
+}
+
+
+
+
+
+
+
+
+
     countOfItem[i].style.display = "inline-block";
     
-        arr_items[i].onclick = counter++
 
         // countOfItem[i].innerHTML = arr_items[0].Count;
     
