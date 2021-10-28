55f323f8dacdb8929f30d3c8adff3c82697d935d
Abdalazez-Azmi <90723420+Abdalazez-Azmi@users.noreply.github.com>
2021-10-28 07:27:33 +0300

    edit

--- problems.js
+++ problems.js
@@ -1,10 +1,6 @@
-//// Main + -
-
+/// db_4 Get Products
 /// Search Product + -
-
+//// Main + -
 /// Calculate Main Total in Main
-
-/// db_4 
-
-/// Accounts in Search Products
-
+/// DATA in Search Products
+/// ports in js works with 1 nodemon https://stackoverflow.com/questions/19296797/running-node-js-http-server-on-multiple-ports
